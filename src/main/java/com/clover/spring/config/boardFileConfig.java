dc9f34f5365abb932e98b384ec8cef8f76b20e5d
J3L2404 <drownerss@gmail.com>
2021-10-06 17:50:39 +0900

    유저 커밋

--- src/main/java/com/clover/spring/config/boardFileConfig.java
+++ src/main/java/com/clover/spring/config/boardFileConfig.java
@@ -2,8 +2,11 @@ package com.clover.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -43,7 +46,13 @@ public class boardFileConfig extends WebMvcConfigurationSupport {
 	    registry.addResourceHandler("/**")
 	        .addResourceLocations("classpath:/static/")
 	        .addResourceLocations("file:" + Consts.FILE_STORAGE_ROOT + "/file/board");
+	    	
 	    super.addResourceHandlers(registry);
 	}
 	
+	@Bean
+	public RequestContextListener requestContextListener() {
+	    return new RequestContextListener();
+	}
+
 }
